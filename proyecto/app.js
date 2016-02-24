c146cb92dcf1873f5771592d5dff5a628f6314e8
Kevin Garibo <garibokevin@gmail.com>
2016-02-23 22:57:22 -0600

    Sessions

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
+var session = require("express-session");
+
 var app  = express();
 
 app.use('/estatico', express.static('public'));
@@ -8,10 +10,17 @@ app.use(express.static('assets'));
 app.use(bodyParser.json()); //para peticiones aplication/json
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+	secret: "123456hub",
+	resave: false,
+	saveuninitialized: false
+}));
+
 app.set("view engine","jade");
 
 app.get("/",function(req,res) {
 	res.render("index");
+	console.log(req.session.user_id);
 });
 
 app.get("/signup",function(req,res) {
@@ -46,8 +55,8 @@ app.post("/users",function(req,res) {
 
 
 app.post("/sessions",function(req,res) {
-	User.findOne({email: req.body.email, password: req.body.password}, function(err,docs){
-		console.log(docs);
+	User.findOne({email: req.body.email, password: req.body.password}, function(err,user){
+		req.session.user_id = user._id;
 		res.send("Hola mundo");
 	})
 });
